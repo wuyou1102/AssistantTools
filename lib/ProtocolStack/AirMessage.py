2c9960555a0286a771d5a37cae53d79fe6425336
wuyou1102 <jotey@qq.com>
2018-04-23 21:11:01 +0800

    add

---
+++ lib/ProtocolStack/AirMessage.py
@@ -0,0 +1,40 @@
+# -*- encoding:UTF-8 -*-
+import six
+from re import compile
+
+__trace_begin_pattern = compile(r' Trace <(.*?)> air message begin ')
+__trace_end_pattern = compile(r' Trace <(.*?)> air message end ')
+trace_pattern = (__trace_begin_pattern, __trace_end_pattern)
+__tdace_begin_pattern = compile(r' Tdace <(.*?)> air message begin ')
+__tdace_end_pattern = compile(r' Tdace <(.*?)> air message end ')
+tdace_pattern = (__tdace_begin_pattern, __tdace_end_pattern)
+
+
+class AirMessageField(str):
+    def __new__(cls, name, target, source, message='', ):
+        obj = str.__new__(cls, name)
+        obj.MSG = message
+        obj.TARGET = target
+        obj.SOURCE = source
+        return obj
+
+
+class AirMessageMetaClass(type):
+    def __new__(cls, name, bases, namespace):
+        code_message_map = {}
+        for k, v in namespace.items():
+            if getattr(v, '__class__', None) and isinstance(v, AirMessageField):
+                if code_message_map.get(v):
+                    raise ValueError("Duplicated name {0} {1}".format(k, v))
+                code_message_map[v] = getattr(v, 'MSG', "")
+        namespace["CODE_MESSAGE_MAP"] = code_message_map
+        return type.__new__(cls, name, bases, namespace)
+
+
+class BaseAirMessage(six.with_metaclass(AirMessageMetaClass)):
+    CODE_MESSAGE_MAP = NotImplemented
+
+
+W = AirMessageField(name="hello", source="a", target="b", message="hello")
+HELLO = AirMessageField(name="world", source="a", target="b", message="world")
+aaaa = AirMessageField(name="hello", source="a", target="b", message="world")
--- lib/UserInterface/Notebook/OfflineTools.py
+++ lib/UserInterface/Notebook/OfflineTools.py
@@ -4,6 +4,9 @@ from NotebookBase import NotebookBase
 from lib import Utility
 from wx import CallAfter
 import time
+from wx import dataview as DV
+from lib.ProtocolStack import AirMessage
+import re
 
 Logger = Utility.getLogger(__name__)
 
@@ -18,61 +21,86 @@ class OfflineTools(NotebookBase):
         self.log_picker = wx.FilePickerCtrl(self, wx.ID_ANY, wx.EmptyString, "Select a file",
                                             "Log files (*.log)|*.log|All files (*.*)|*.*", wx.DefaultPosition,
                                             wx.DefaultSize, wx.FLP_DEFAULT_STYLE)
-        analysis_button = wx.Button(self, wx.ID_ANY, "Analysis", wx.DefaultPosition, wx.DefaultSize, 0)
-        analysis_button.Bind(wx.EVT_BUTTON, self.__analysis_log)
+        self.analysis_button = wx.Button(self, wx.ID_ANY, "Analysis", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.analysis_button.Bind(wx.EVT_BUTTON, self.__analysis_log)
         PickerSizer.Add(self.log_picker, 1, wx.ALL, 1)
-        PickerSizer.Add(analysis_button, 0, wx.ALL, 1)
+        PickerSizer.Add(self.analysis_button, 0, wx.ALL, 1)
 
-        self.DVTC = DV.DataViewTreeCtrl(self, wx.ID_ANY,
-                                        style=DV.DV_ROW_LINES | DV.DV_VERT_RULES)
-        tree = self.DVTC
-        il = wx.ImageList(16, 16)
-        il.Add((wx.ArtProvider.GetBitmap(wx.ART_CDROM, wx.ART_OTHER, (16, 16))))
-        il.Add((wx.ArtProvider.GetBitmap(wx.ART_ERROR, wx.ART_OTHER, (16, 16))))
-
-        tree.AssignImageList(il)
-
-        for d in range(3):
-            itemd = tree.AppendContainer(DV.NullDataViewItem, text='dddddd', icon=1, expanded=1, data="asd")
-            for c in range(10):
-                itemc = tree.AppendContainer(itemd, 'cccccccc', 0)
-                for b in range(10):
-                    itema = tree.AppendItem(itemc, text='aaaaaaaaaaaaa', icon=0,  data="asd")
-
-        self.SetSizer(mainSizer)
-
-                    # self.DVTC.Create(self)
-        # container = self.DVTC.AppendContainer(self.DVTC, text="ss")
+        self.DVLC = DV.DataViewListCtrl(self, wx.ID_ANY)
+        self.DVLC.AppendTextColumn(u"时间")
+        self.DVLC.AppendTextColumn(u"优先级")
+        self.DVLC.AppendTextColumn(u"消息名")
+        self.DVLC.AppendTextColumn(u"消息源")
+        self.DVLC.AppendTextColumn(u"目标源")
+        self.DVLC.AppendProgressColumn(u"信号")
         LogAnalysisSizer.Add(PickerSizer, 0, wx.EXPAND)
-        LogAnalysisSizer.Add(self.DVTC, 1, wx.EXPAND | wx.ALL, 1)
+        LogAnalysisSizer.Add(self.DVLC, 1, wx.EXPAND | wx.ALL, 1)
         MainSizer.Add(LogAnalysisSizer, 1, wx.EXPAND, 5)
         self.SetSizer(MainSizer)
 
     def __analysis_log(self, event):
         def analysis(log_file):
-            line_num = 0
-            with open(log_file) as log:
-                for line in log.readlines():
-                    line_num += 1
-                    data = [True, line, line_num]
-<<<<<<< HEAD
-                    CallAfter(self.data_ctrl.AppendItem, data)
-<<<<<<< HEAD
-=======
-                    if line_num % 999 == 0:
-                        time.sleep(0.1)
-=======
-                    CallAfter(self.DVTC.AppendItem, data)
->>>>>>> 47c8ca939bade19d0d07a985c031dd03dfbd38ce
->>>>>>> parent of 706b853... Revert "Merge branch 'master' of https://github.com/wuyou1102/AssistantTools"
 
+            with open(log_file) as mLog:
+                lines = mLog.readlines()
+            split_logs = self.__split_log(lines, [AirMessage.trace_pattern])
+            for log in split_logs:
+                first = lines[log[0]]
+                data = [first[1], first[2], first[3], first[4], first[5], first[6]]
+                CallAfter(self.DVLC.AppendItem, data)
+
+            self.analysis_button.Enable()
+
+
+        self.analysis_button.Disable()
         log_file = self.log_picker.GetPath()
         if Utility.exists(log_file):
             Utility.append_work(target=analysis, log_file=log_file)
         else:
             Logger.warn('\"%s\" does not exist.' % log_file)
+            self.analysis_button.Enable()
 
-    def asd(self):
-        for x in range(999):
-            data = [True, "hello world:%s" % x, x]
-            CallAfter(self.DVTC.AppendItem, data)
+    @staticmethod
+    def __split_log(lines, patterns):
+        def find_end(s_number, e_pattern):
+            for current_number in xrange(s_number, length):
+                line = lines[current_number]
+                if re.search(e_pattern, line):
+                    return s_number, current_number
+            return s_number, None
+
+        def find_log(s_number):
+            for current_number in xrange(s_number, length):
+                line = lines[current_number]
+                for s_pattern, e_pattern in patterns:
+                    if re.search(s_pattern, line):
+                        return find_end(s_number=current_number, e_pattern=e_pattern)
+            return None, None
+
+        length = len(lines)
+        logs = list()
+        line_number = 0
+        while line_number < length:
+            start_line, end_line = find_log(line_number)
+            if not start_line or not end_line:
+                break
+            line_number = end_line + 1
+            logs.append((start_line, end_line))
+        return logs
+
+
+class Log(object):
+    def __init__(self, path):
+        with open(path) as mLog:
+            lines = mLog.readlines()
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        if self.n < self.max:
+            r = self.b
+            self.a, self.b = self.b, self.a + self.b
+            self.n = self.n + 1
+            return r
+        raise StopIteration()
