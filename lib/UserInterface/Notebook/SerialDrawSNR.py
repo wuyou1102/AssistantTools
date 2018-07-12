f69542e00b7b300b9bbd3e02ffa6de09f4d0a34e
wuyou1102 <jotey@qq.com>
2018-07-12 16:34:29 +0800

    add

--- lib/UserInterface/Notebook/SerialDrawSNR.py
+++ lib/UserInterface/Notebook/SerialDrawSNR.py
@@ -1,11 +1,10 @@
 # -*- encoding:UTF-8 -*-
-# -*- encoding:UTF-8 -*-
 import wx
 from NotebookBase import NotebookBase
 from lib import Utility
 from SimpleMatplotPanel import MatplotPanel
 from lib.Instrument import Serial
-from time import sleep
+from lib.UserInterface.Dialog import DialogBase
 from matplotlib import animation
 import numpy as np
 import re
@@ -28,20 +27,20 @@ class SerialDrawSNR(NotebookBase):
         self.session = None
         self.DC = None
         self.lines = list()
-        self.mAnim1 = None
-        self.mAnim2 = None
-        self.mAnim3 = None
+        self.anim_bler = None
+        self.anim_snr = None
+        self.anim_rssi = None
         B_Refresh = wx.Button(self, wx.ID_ANY, u"Refresh", wx.DefaultPosition, (60, 27), 0)
         B_Refresh.Bind(wx.EVT_BUTTON, self.on_refresh)
         B_Connect = wx.Button(self, wx.ID_ANY, u"Connect", wx.DefaultPosition, (-1, 27), 0)
         B_Connect.Bind(wx.EVT_BUTTON, self.on_connect)
-        # B_New = wx.Button(self, wx.ID_ANY, u"+", wx.DefaultPosition, (30, 27), 0)
-        # B_New.Bind(wx.EVT_BUTTON, self.on_new_window)
+        B_New = wx.Button(self, wx.ID_ANY, u"+", wx.DefaultPosition, (30, 27), 0)
+        B_New.Bind(wx.EVT_BUTTON, self.create_output)
 
         PortsSizer.Add(B_Refresh, 0, wx.ALIGN_CENTER_VERTICAL)
         PortsSizer.Add(self.C_ports, 0, wx.ALIGN_CENTER_VERTICAL | wx.ALL, 1)
         PortsSizer.Add(B_Connect, 0, wx.ALIGN_CENTER_VERTICAL)
-        # PortsSizer.Add(B_New, 0, wx.ALIGN_CENTER_VERTICAL)
+        PortsSizer.Add(B_New, 0, wx.ALIGN_CENTER_VERTICAL)
         self.MPL_RSSI = MatplotPanel(self, 0, 50, -120, -30)
         self.MPL_BLER = MatplotPanel(self, 0, 50, 0, 100)
         self.MPL_SNR = MatplotPanel(self, 0, 50, -10, 30, ya=5, yi=1)
@@ -56,8 +55,19 @@ class SerialDrawSNR(NotebookBase):
 
         self.SetSizer(MainSizer)
 
-    def on_connect(self, event):
+    def create_output(self, event):
+        if self.session and self.DC:
+            dialog = OutputDialog(size=(700, 560), name="", session=self.session, tearDown=self.close_output)
+            self.DC.set_output(dialog.LOG_OUTPUT)
+            dialog.Show()
+        else:
+            Logger.info(u"Nothing need output.")
 
+    def close_output(self):
+        if self.DC:
+            self.DC.reset_output()
+
+    def on_connect(self, event):
         obj = event.GetEventObject()
         state = obj.GetLabel()
         if state == "Connect":
@@ -76,44 +86,14 @@ class SerialDrawSNR(NotebookBase):
                 obj.SetLabel("Disconnect")
             except IOError:
                 Logger.error(u"At the same time, the same serial port can only be used by one device.")
-
-        elif state == "Disconnect":
-            if self.session:
-                self.session.close()
-            obj.SetLabel("Connect")
         else:
-            if self.session:
-                self.session.close()
+            self.StopDraw()
             obj.SetLabel("Connect")
 
     def on_refresh(self, event):
         self.C_ports.Items = Serial.get_ports()
 
-    def StartDraw(self):
-        self.mAnim1 = animation.FuncAnimation(self.MPL_BLER.Figure, self.Update, frames=100,
-                                              interval=500,
-                                              blit=True)
-        self.mAnim2 = animation.FuncAnimation(self.MPL_SNR.Figure, self.Update, frames=100,
-                                              interval=500,
-                                              blit=True)
-        self.mAnim3 = animation.FuncAnimation(self.MPL_RSSI.Figure, self.Update, frames=100,
-                                              interval=500,
-                                              blit=True)
-
-    def StopDraw(self):
-        Logger.info('StopDrawLine')
-        if self.mAnim1:
-            self.mAnim1._stop()
-            self.mAnim1 = None
-        if self.mAnim2:
-            self.mAnim2._stop()
-            self.mAnim2 = None
-        if self.mAnim3:
-            self.mAnim3._stop()
-            self.mAnim3 = None
-        if self.DC:
-            self.DC.stop()
-
+    def reset_mpl(self):
         self.MPL_RSSI.cla()
         self.MPL_RSSI.init_axis()
         self.MPL_SNR.cla()
@@ -121,11 +101,42 @@ class SerialDrawSNR(NotebookBase):
         self.MPL_BLER.cla()
         self.MPL_BLER.init_axis()
 
+    def start_animation(self):
+        self.anim_bler = animation.FuncAnimation(self.MPL_BLER.Figure, self.Update, frames=100, interval=500, blit=True)
+        self.anim_rssi = animation.FuncAnimation(self.MPL_RSSI.Figure, self.Update, frames=100, interval=500, blit=True)
+        self.anim_snr = animation.FuncAnimation(self.MPL_SNR.Figure, self.Update, frames=100, interval=500, blit=True)
+
+    def stop_animation(self):
+        Logger.debug('StopAnimation')
+        if self.anim_bler:
+            self.anim_bler._stop()
+            self.anim_bler = None
+        if self.anim_snr:
+            self.anim_snr._stop()
+            self.anim_snr = None
+        if self.anim_rssi:
+            self.anim_rssi._stop()
+            self.anim_rssi = None
+
+    def stop_data_collect(self):
+        if self.DC:
+            self.DC.stop()
+            self.DC = None
+        if self.session:
+            self.session.close()
+            self.session = None
+
+    def StartDraw(self):
+        self.reset_mpl()
+        self.start_animation()
+
+    def StopDraw(self):
+        Logger.debug('StopDrawLine')
+        self.stop_animation()
+        self.stop_data_collect()
+
     def Update(self, i):
         return self.lines
-
-
-from lib.UserInterface.Dialog import DialogBase
 
 
 class DataCollect(object):
@@ -135,16 +146,25 @@ class DataCollect(object):
         self.B = B
         self.session = session
         self.flag = True
+        self.output = None
         Utility.append_work(target=self.COLLECT_DATA)
 
     def stop(self):
         self.flag = False
+
+    def set_output(self, output):
+        self.output = output
+
+    def reset_output(self):
+        self.output = None
 
     def COLLECT_DATA(self):
         while self.session.is_open() and self.flag:
             line = self.session.read_line()
             if not line:
                 continue
+            if self.output:
+                self.output(line)
             if line.startswith("br ("):
                 try:
                     br, fch, ds, mode = Utility.find_in_string(bler_pattern, line)
@@ -294,10 +314,69 @@ class Line(object):
     def GetLine(self):
         return self._line
 
-    # def Init(self):
-    #     self._singal = np.array([])
-    #     self._sequence = np.array([])
-    #     self._line.set_data(self._sequence, self._singal, )
-
     def get_count(self):
         return self.count
+
+
+class OutputDialog(DialogBase.DialogBase):
+    def __init__(self, size, session, tearDown, name="", positon=wx.DefaultPosition):
+        DialogBase.DialogBase.__init__(self, name=name, size=size, pos=positon)
+        MainSizer = wx.BoxSizer(wx.VERTICAL)
+        self.session = session
+        self.filters = list()
+        self.tearDown = tearDown
+        FilterSizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.filter_tc = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0)
+        B_OK = wx.Button(self, wx.ID_ANY, u"OK", wx.DefaultPosition, (40, 32), 0)
+        B_OK.Bind(wx.EVT_BUTTON, self.on_ok)
+
+        FilterSizer.Add(self.filter_tc, 1, wx.EXPAND)
+        FilterSizer.Add(B_OK, 0, wx.ALIGN_CENTER_VERTICAL)
+        OutputSizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.log_tc = wx.TextCtrl(self, wx.ID_ANY, '',
+                                  style=wx.TE_MULTILINE | wx.TE_READONLY | wx.HSCROLL)
+        self.log_tc.SetInsertionPointEnd()
+        self.log_tc.SetFont(wx.Font(9, wx.SWISS, wx.NORMAL, wx.NORMAL, False))
+        OutputSizer.Add(self.log_tc, 1, wx.EXPAND)
+        InputSizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.input_tc = wx.TextCtrl(self, wx.ID_ANY, '', style=wx.TE_MULTILINE | wx.TE_PROCESS_ENTER)
+        self.input_tc.SetFont(wx.Font(9, wx.SWISS, wx.NORMAL, wx.NORMAL, False))
+        self.input_tc.Bind(wx.EVT_TEXT_ENTER, self.after_enter)
+        InputSizer.Add(self.input_tc, 1, wx.EXPAND)
+
+        MainSizer.Add(FilterSizer, 0, wx.EXPAND | wx.ALL, 5)
+        MainSizer.Add(OutputSizer, 4, wx.EXPAND | wx.ALL, 5)
+        MainSizer.Add(InputSizer, 1, wx.EXPAND | wx.ALL, 5)
+        self.SetSizer(MainSizer)
+        self.Bind(wx.EVT_CLOSE, self.on_close)
+
+    def on_ok(self, event):
+        string = self.filter_tc.GetValue()
+        if "|" in string:
+            self.filters = string.split('|')
+        else:
+            self.filters = [string]
+        print self.filters
+
+    def LOG_OUTPUT(self, line):
+        msg = '{time}:{line}\n'.format(time=Utility.get_timestamp(), line=line)
+        if self.filters:
+            for f in self.filters:
+                if line.startswith(f):
+                    wx.CallAfter(self.log_tc.AppendText, msg)
+        else:
+            wx.CallAfter(self.log_tc.AppendText, msg)
+
+    def after_enter(self, event):
+        values = self.input_tc.GetValue()
+        print repr(values)
+        for cmd in values.split('\n'):
+            if self.session:
+                self.session.send_command(cmd)
+        self.input_tc.SetValue('')
+        event.Skip()
+
+    def on_close(self, event):
+        Logger.info(u"Close Output Dialog")
+        self.tearDown()
+        event.Skip()
